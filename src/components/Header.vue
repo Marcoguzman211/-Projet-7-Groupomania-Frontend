6fa6ae61883e25e4f9e679be5f40cdee60e73c2b
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2021-03-06 23:06:55 +0100

    Fonctions Login et Signup

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+        <v-img
+        class="mx-auto mb-8"
+        src="../assets/icon-left-font-removebg-preview.png"
+        max-width="600"
+        max-height="150"
+        alt='Logo de Groupomania'
+        ></v-img>
+</template>
---
+++ src/views/Signup.vue
@@ -0,0 +1,155 @@
+<template>
+    <div class="background">
+    <div class="container mx-auto">
+        <!--img src="../assets/icon-above-font-removebg-preview.png" alt="Groupomania logo"-->
+        <Header />
+        <form @submit.prevent = signup()>
+            <div class="mb-2 center">Champs requis (*)</div>
+            <input id="nom" ref="nom" type="text" placeholder="Nom"  title="Renseignez votre nom">
+            <input id="prenom" ref="prenom" type="text" placeholder="Prénom" title="Renseignez votre prénom">
+            <input id="email" ref="email" type="email" placeholder="E-mail (*)" title="Renseignez votre email" required>
+            <input id="password" ref="password" type="password" placeholder="Mot de passe (*)" title="Renseignez votre mot de passe" required>
+            <input id="bio" ref="bio" type="text" placeholder="Bio" title="Renseignez une bio">
+            <div class="message-erreur">{{ message }}</div>
+            <div class="mx-auto mt-6 mb-15">
+              <button id="signup" type="submit" class="mx-5">Inscription</button>
+              <router-link to="/" id="login" tag="button" class="mx-5">Connexion</router-link>
+            </div>
+        </form>
+    </div>
+  </div>
+</template>
+<script>
+import { notConnectedClient } from '../services/auth.js';
+import Header from '../components/Header.vue';
+
+export default {
+    name: "Signup",
+    components: {
+        Header
+    } ,
+    data() {
+        return {
+            message: ""
+        }
+    },
+    created(){
+        this.connectedUser()
+    },
+    methods: {
+        connectedUser(){
+            if(localStorage.groupomaniaUser === undefined) {
+                this.approuvedConnexion = false;
+                console.log('Utilisateur non connecté !')
+            } else {
+                this.approuvedConnexion = true;
+                console.log('Utilisateur connecté!')
+                location.href = "/"
+            }
+        },
+        signup(){
+            const nom = this.$refs.nom.value
+            const prenom = this.$refs.prenom.value
+            const password = this.$refs.password.value
+            const email = this.$refs.email.value
+            const bio = this.$refs.bio.value
+            
+            if (email && password){
+
+                notConnectedClient.post('/users/signup', {
+                nom,
+                prenom,
+                email,
+                password,
+                bio
+            }).then((res) => {
+                if (res.status === 201){
+                    const groupomaniaUser = {
+                        token: res.data.token
+                    }
+                    localStorage.setItem('groupomaniaUser', JSON.stringify(groupomaniaUser))
+                    this.$router.push({name: 'Home' })
+                }
+            })
+            .catch((error)=> {
+                this.message = error.response.data.error
+            })
+        }
+      }
+    }
+}
+
+</script>
+<style>
+.background{
+      background-color: #f0f0f0;
+      min-height: 100vh;
+    }
+
+    .container{
+        max-width: 500px;
+        margin: 0;
+        padding: 0;
+    }
+
+    .center {
+        margin: 0 auto;
+        font-weight: bold;
+    }
+    
+    form{
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+          align-items: baseline;
+          margin: 0 auto;
+    }
+
+    form input{
+        margin: auto;
+        width: 80%;
+        font-size: 1.05rem;
+        padding: 10px;
+        margin-bottom: 15px;
+        text-align: center;
+        background-color: white;
+        border: 1px rgba(0, 0, 0, 0.548) solid;
+        border-radius: 15px;
+    }
+
+    #signup{
+        padding: 6px 12px;
+        font-size: 1.5rem;
+        color: #ffffff;
+        background-color: #122441;
+        border: none;
+        border-radius: 10px;
+        transition-duration: 0.2s;
+    }
+
+    #signup:hover{
+        transform: scale(1.1);
+    }
+
+    #login{
+        padding: 6px 12px;
+        font-size: 1rem;
+        color: #ffffff;
+        background-color: #fd2d01;
+        border: none;
+        border-radius: 10px;
+        transition-duration: 0.2s;
+    }
+
+    #login:hover{
+        transform: scale(1.1);
+    }
+    
+    .message-erreur{
+        text-align: center;
+        margin: auto;
+        color: red;
+        font-size: 1rem;
+    }
+
+</style>
